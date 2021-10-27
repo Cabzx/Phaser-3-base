ecc859e9090c4a1b6f52a3b4b458cdf9953e1ada
Cabzx <cabzx@hotmail.com>
2021-10-27 01:22:04 -0500

    initial commit

---
+++ scenes/Scene2.js
@@ -0,0 +1,19 @@
+class Scene2 extends Phaser.Scene{
+
+    constructor(){
+        super("sceneName2");
+    }
+     
+    create(){
+
+    }
+
+    preload(){
+
+    }
+
+    update(){
+
+    }
+
+}
